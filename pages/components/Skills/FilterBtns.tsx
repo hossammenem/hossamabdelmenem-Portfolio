a77e31e07bdb06890cd86ede42c4af214f41c492
hossammenem <hossammenem0102840@gmail.com>
2023-03-17 16:32:52 +0200

    solving the type error that was in FilterBtns.tsx

--- pages/components/Skills/FilterBtns.tsx
+++ pages/components/Skills/FilterBtns.tsx
@@ -8,7 +8,7 @@ export default function FilterBtn(props: IFilterBtn) {
   return (
     <button
       className={`${isAll} filter-btn rounded-md px-2 py-1 transition-all duration-300 ease-in-out hover:bg-gray-500 focus-visible:outline-none`}
-      onClick={props.filterFunc}
+      onClick={() => props.filterFunc}
     >
       {props.filterName}
     </button>
