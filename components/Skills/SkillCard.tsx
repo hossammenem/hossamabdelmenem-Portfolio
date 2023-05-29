82f272fb8a50678297f7c32c7ff25d8e3dfba248
hossammenem <hossammenem0102840@gmail.com>
2023-05-29 23:04:09 +0300

    changing skill card width

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -16,7 +16,7 @@ export const SkillCard = ({ name, img, alt }: ISkill) => {
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex h-[120px] w-[120px] flex-col items-center justify-center gap-2 rounded-md bg-anotherBlack p-4 text-xl font-bold shadow-md transition-all duration-100 ease-in hover:bg-gray-700 md:h-[225px] md:w-[225px] md:gap-4 md:text-4xl"
+      className="flex h-[120px] w-[120px] flex-col items-center justify-center gap-2 rounded-md bg-anotherBlack p-4 text-xl font-bold shadow-md transition-all duration-100 ease-in hover:bg-gray-700 md:h-[200px] md:w-[200px] md:gap-4 md:text-3xl"
     >
       <div className="h-[60px] w-[60px] md:h-[100px] md:w-[100px]">
         <img src={img} alt={alt} />
